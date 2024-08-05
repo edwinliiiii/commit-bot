fe95572ff16d2833e555d8a276a893ae0370ee22
edwinliiiii <91173669+edwinliiiii@users.noreply.github.com>
2024-08-05 14:12:47 -0400

    cronjob to every other hour

--- bot.sh
+++ bot.sh
@@ -16,5 +16,5 @@ git commit -m "The date is $info"
 git push
 cd ~
 
-# crontab runs script every 0th minute of every hour :)
-# 00 * * * * /bin/bash ~/commit-bot/bot.sh
+# crontab runs script every 0th minute of every other hour :)
+# 00 */2 * * * /bin/bash ~/commit-bot/bot.sh
